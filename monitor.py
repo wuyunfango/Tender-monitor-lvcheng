dbb6bf02d002132d6008e42bf04bc30747829cad
Vicky Wu <62053512+wuyunfango@users.noreply.github.com>
2026-05-10 12:32:48 +1000

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,9 +1,28 @@
 import requests
-from bs4 import BeautifulSoup
+import hashlib
+import os
 
-url = "https://lczc.gtcloud.cn/portal/biding"
+url = "https://lczc.gtcloud.cn"
 
 response = requests.get(url)
+html = response.text
 
-print(response.status_code)
-print("Website checked successfully")
+# 用 hash 判断是否变化
+new_hash = hashlib.md5(html.encode()).hexdigest()
+
+# 读取旧记录
+if os.path.exists("last_hash.txt"):
+    with open("last_hash.txt", "r") as f:
+        old_hash = f.read()
+else:
+    old_hash = ""
+
+# 判断是否更新
+if new_hash != old_hash:
+    print("🚨 新内容更新了！")
+
+    # 保存新状态
+    with open("last_hash.txt", "w") as f:
+        f.write(new_hash)
+else:
+    print("没有更新")
